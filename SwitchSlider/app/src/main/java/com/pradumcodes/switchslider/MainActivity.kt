623aff3a3fdb94210640153a57897295bcf910ba
Pradum Kumar <pradumky803@gmail.com>
2025-12-05 21:55:39 +0530

    Form using outlinedTextField

--- SwitchSlider/app/src/main/java/com/pradumcodes/switchslider/MainActivity.kt
+++ SwitchSlider/app/src/main/java/com/pradumcodes/switchslider/MainActivity.kt
@@ -41,8 +41,9 @@ class MainActivity : ComponentActivity() {
             MaterialTheme {
                 Surface {
                     Column {
-                        SimpleContent()
-                        SimpleCheckBox()
+//                        SimpleContent()
+//                        SimpleCheckBox()
+                        Form()
                     }
                 }
             }
